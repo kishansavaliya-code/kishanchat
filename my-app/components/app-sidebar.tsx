0dd52ce67077b6344acd7c609fd5082452853bed
kishansavaliya-code <savaliyakishan71@gmail.com>
2025-06-23 18:47:20 +0530

    Add files via upload

---
+++ my-app/components/app-sidebar.tsx
@@ -0,0 +1,51 @@
+import * as React from "react"
+import { MessagesSquare } from "lucide-react"
+import Link from "next/link"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarRail,
+} from "@/components/ui/sidebar"
+import { ThreadList } from "./assistant-ui/thread-list"
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <Link href="#" className="flex items-center gap-2">
+                <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+                  <MessagesSquare className="size-4" />
+                </div>
+                <div className="flex flex-col gap-0.5 leading-none">
+                  <span className="font-semibold">SK CHAT</span>
+                </div>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <ThreadList />
+      </SidebarContent>
+
+      <SidebarRail />
+      <SidebarFooter>
+        <SidebarMenu>
+
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
+    </Sidebar>
+  )
+}
