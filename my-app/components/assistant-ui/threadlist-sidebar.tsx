4009035d25b87a95aa6f824fe4c1d6c4d9ceb23f
kishansavaliya-code <savaliyakishan71@gmail.com>
2025-09-14 10:34:23 +0530

    Add files via upload

---
+++ my-app/components/assistant-ui/threadlist-sidebar.tsx
@@ -0,0 +1,58 @@
+import * as React from "react";
+import { Github, MessagesSquare } from "lucide-react";
+import Link from "next/link";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarRail,
+} from "@/components/ui/sidebar";
+import { ThreadList } from "@/components/assistant-ui/thread-list";
+
+export function ThreadListSidebar({
+  ...props
+}: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar {...props}>
+      <SidebarHeader className="aui-sidebar-header mb-2 border-b">
+        <div className="aui-sidebar-header-content flex items-center justify-between">
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton size="lg" asChild>
+                <Link
+                  href=""
+                  rel="noopener noreferrer"
+                >
+                  <div className="aui-sidebar-header-icon-wrapper flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+                    <MessagesSquare className="aui-sidebar-header-icon size-4" />
+                  </div>
+                  <div className="aui-sidebar-header-heading mr-6 flex flex-col gap-0.5 leading-none">
+                    <span className="aui-sidebar-header-title font-semibold">
+                      SK CHAT
+                    </span>
+                  </div>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </div>
+      </SidebarHeader>
+      <SidebarContent className="aui-sidebar-content px-2">
+        <ThreadList />
+      </SidebarContent>
+      <SidebarRail />
+      <SidebarFooter className="aui-sidebar-footer border-t">
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
